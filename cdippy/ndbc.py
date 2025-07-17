95afb8cd6e3abdd2dca9f6cb93b8cc2419cb7694
haileyajohnson <haj022@ucsd.edu>
2025-07-16 19:06:50 -0700

    fix docstrings for ndbc

--- cdippy/ndbc.py
+++ cdippy/ndbc.py
@@ -1,4 +1,6 @@
-"""Methods for working with NDBC"""
+"""
+Convenience methods for working with NDBC stations.
+"""
 
 import os
 from datetime import datetime, timezone
@@ -17,7 +19,12 @@ cdip_base = "https://cdip.ucsd.edu"
 
 
 def get_stn_info(wmo_id):
-    """Work in progress, querying ndbc sos service."""
+    """
+    *Work in progress*:  querying ndbc sos service.
+
+    Args:
+        wmo_id (str): The WMO id of the station.
+    """
     qry = "&".join([request, service, version, outputformat, describe_stn + wmo_id])
     url = "?".join([sos_base, qry])
     root = uu.load_et_root(url)
@@ -25,13 +32,19 @@ def get_stn_info(wmo_id):
     uu.rfindt(root, results, "description")
 
 
-def get_wmo_id(
-    stn,
-    store=True,
-    filepath=".",
-):
-    """Queries cdip wmo id table for a given station. Drops pickle file locally."""
-    pkl_fl = filepath + "/WMO_IDS.pkl" if store else None
+def get_wmo_id(stn):
+    """
+    Queries cdip table of WMO ids for the id of a given station. Optionally stores the table as a pickle file.
+
+    Args:
+        stn (str): CDIP 3 digit id.
+        store (bool): Whether to store the table locally. Defaults to `True`.
+        filepath (str): Where to store the WMO id table locally. Does nothing if `store=False`. Defaults to ".".
+
+    Returns:
+        id (str): The NDBC id for the station as a string or `None` if it is not found.
+    """
+    pkl_fl = "./WMO_IDS.pkl"
     now = datetime.now(timezone.utc)
     if not pkl_fl or now.minute == 23 or not os.path.isfile(pkl_fl):
         url = "/".join([cdip_base, "wmo_ids"])
@@ -45,5 +58,4 @@ def get_wmo_id(
         ids = cu.pkl_load(pkl_fl)
     if stn in ids:
         return ids[stn]
-    else:
-        return None
+    return None
